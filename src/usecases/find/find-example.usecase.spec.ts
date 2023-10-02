3868902b2426837c7a66b3b0cb76f16b476fcca3
CaiqueRibeiro <ribeiro.caique95@gmail.com>
2023-10-02 18:03:06 -0300

    tests: add exception test cenario

--- src/usecases/find/find-example.usecase.spec.ts
+++ src/usecases/find/find-example.usecase.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it, vitest } from 'vitest'
 import { FindExampleUseCase } from './find-example.usecase'
 import { ExamplesRepositoryInterface } from '@/repositories/examples-repository.interface'
+import { ResourceNotFoundError } from '@/errors/resource-not-found.error'
 
 describe('FindExampleUseCase unit tests', () => {
   let examplesRepository: ExamplesRepositoryInterface
@@ -17,5 +18,9 @@ describe('FindExampleUseCase unit tests', () => {
     examplesRepository.findById = vitest.fn().mockResolvedValue('returned-example')
     const result = await usecase.execute({ id: '0ce2d59d-0aab-4f02-ad25-84cc5f75a8f8' })
     expect(result).toBe('returned-example')
+  })
+
+  it('should throw error if example was not found', async () => {
+    await expect(usecase.execute({ id: '1' })).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
